6e0507031d75d317b45bfd035afb41870969022c
Reagan McFarland <reagan.mcfarland1059@gmail.com>
2025-05-31 17:19:03 -0400

    add highlight on yank

---
+++ .config/nvim/lua/config/autocmds.lua
@@ -0,0 +1,7 @@
+-- highlight text on yank
+vim.api.nvim_exec([[
+  augroup YankHighlight
+    autocmd!
+    autocmd TextYankPost * silent! lua vim.highlight.on_yank{higroup="IncSearch", timeout=500}
+  augroup end
+]], false)
